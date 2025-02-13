0d0d6925eff4e9cd9492a2cbfd56deaaf522184c
Monisha Sivakumar <115549447+monishaa2@users.noreply.github.com>
2025-02-13 19:32:35 +0530

    Update Main.java

--- 12-2-2025/serializable/src/Main.java
+++ 12-2-2025/serializable/src/Main.java
@@ -13,14 +13,14 @@ public class Main {
       public static void serialize()
       {
           try {
-              //Creating the object
+             
               Student s1 = new Student(211, "ravi");
-              //Creating stream and writing the object
+            
               FileOutputStream fout = new FileOutputStream("f.txt");
               ObjectOutputStream out = new ObjectOutputStream(fout);
               out.writeObject(s1);
               out.flush();
-              //closing the stream
+             
               out.close();
               System.out.println("success");
           } catch (Exception e) {
@@ -29,12 +29,12 @@ public class Main {
       }
       public static void deserialize() {
           try {
-              //Creating stream to read the object
+              
               ObjectInputStream in = new ObjectInputStream(new FileInputStream("f.txt"));
               Student s = (Student) in.readObject();
-              //printing the data of the serialized object
+             
               System.out.println(s.rno + " " + s.name);
-              //closing the stream
+              
               in.close();
           } catch (Exception e) {
               System.out.println(e);
