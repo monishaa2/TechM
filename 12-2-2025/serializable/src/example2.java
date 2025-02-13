26bf9535d62b6845d5cf9c3c231945fb73476eec
Monisha Sivakumar <115549447+monishaa2@users.noreply.github.com>
2025-02-13 19:31:17 +0530

    Update example2.java

--- 12-2-2025/serializable/src/example2.java
+++ 12-2-2025/serializable/src/example2.java
@@ -4,14 +4,14 @@ public class example2 {
     public static void serialize()
     {
         try {
-            //Creating the object
+      
             employee e1 = new employee(21,"anisha","anishahakunakmatata@gmail.com");
-            //Creating stream and writing the object
+            
             FileOutputStream fout = new FileOutputStream("employee.txt");
             ObjectOutputStream out = new ObjectOutputStream(fout);
             out.writeObject(e1);
             out.flush();
-            //closing the stream
+      
             out.close();
             System.out.println("success");
         } catch (Exception e) {
@@ -20,12 +20,12 @@ public class example2 {
     }
     public static void deserialize() {
         try {
-            //Creating stream to read the object
+
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("employee.txt"));
             employee e  = (employee) in.readObject();
-            //printing the data of the serialized object
+      
             System.out.println(e.eno + " " + e.ename);
-            //closing the stream
+
             in.close();
         } catch (Exception e) {
             System.out.println(e);
